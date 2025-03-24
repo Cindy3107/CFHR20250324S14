17b9ce077caa8c2ebcef0806e2353eb0440cf548
cindy hernandez <cindyhernandez310706@outlook.com>
2025-03-24 15:07:17 -0600

    Se modifico la vista Create.cshtml

--- CFHR20250324S14.AppWebMVC/Models/Cliente.cs
+++ CFHR20250324S14.AppWebMVC/Models/Cliente.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 
 namespace CFHR20250324S14.AppWebMVC.Models;
 
@@ -7,9 +8,14 @@ public partial class Cliente
 {
     public int Id { get; set; }
 
+    [Required(ErrorMessage ="El nombre del cliente es obligatorio.")]
+
     public string Nombre { get; set; } = null!;
 
+    [Display(Name = "dirección")]
+
     public string? Direccion { get; set; }
+    [Display(Name = "teléfono")]
 
     public string? Telefono { get; set; }
 
